ed641a834557d830b2d86d96d59c2b0cb90d33f3
puja khadka <khadkapuja195@gmail.com>
2025-12-31 09:07:23 +0545

    travel guide section added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -23,6 +23,9 @@ import { Button } from "../ui/button";
 import { BsThreeDots } from "react-icons/bs";
 import { FaArrowRight } from "react-icons/fa";
 import { MdOutlineStarBorder } from "react-icons/md";
+import { FaFacebook } from "react-icons/fa";
+import { FiTwitter } from "react-icons/fi";
+import { FaInstagram } from "react-icons/fa6";
 
 function Home() {
   const [scrolled, setScrolled] = useState(false);
@@ -511,7 +514,7 @@ const [formdata,setFormdata]=useState(
               backgroundRepeat: "no-repeat",
             }}
           >
-            <div className=" relative z-10 grid grid-cols-2 gap-20 text-white px-10 py-30">
+            <div className=" relative z-5 grid grid-cols-2 gap-20 text-white px-10 py-30">
               <div>
                 <h4 className="font-bold ">Booking</h4>
                 <h1 className="font-bold text-3xl my-4">Online Booking</h1>
@@ -592,8 +595,64 @@ const [formdata,setFormdata]=useState(
         </div>
       </section>
         
-      <section>  
-        
+      <section className="container mx-auto mt-25">  
+         <h3 className="flex items-center justify-center text-green-800 gap-2 font-bold text-xl"> <BsThreeDots /> TRAVEL GUIDE <BsThreeDots /></h3>
+         <h1 className="text-center text-3xl font-bold">Meet Our Guide</h1>
+         <div className="grid grid-cols-4 gap-6 px-20 my-9">
+          <div className=" relative shadow-lg bg-white group overflow-hidden">
+             <img className=" transition-transform duration-300 group-hover:scale-105" src="https://media.istockphoto.com/id/1503232125/photo/happy-smile-and-portrait-of-business-man-in-city-for-professional-corporate-and-pride.jpg?s=612x612&w=0&k=20&c=uPu3GKHW507ruenLY8xwsB3OgOSwLSWjE3fBpy20Hac=" alt="" />
+             <div className="flex items-center justify-center gap-2 absolute top-39 left-20 ">
+               <span className="w-8 h-8 rounded-full border-2 flex items-center justify-center bg-white hover:bg-green-500  "> <FaFacebook className="text-green-700 "/> </span>  
+               <span className="w-8 h-8 rounded-full border-2  flex items-center justify-center bg-white hover:bg-green-500 "> <FiTwitter className="text-green-700"/> </span>  
+               <span className="w-8 h-8 rounded-full border-2  flex items-center justify-center bg-white hover:bg-green-500 "> <FaInstagram className="text-green-700"/> </span>  
+                  
+             </div>
+            <div className="text-center my-8">
+             <h4 className="font-bold">Full Name</h4>
+             <h5 className="text-gray-700">Destination</h5>
+             </div>
+          </div>
+          <div className=" relative shadow-lg bg-white group overflow-hidden">
+             <img className=" transition-transform duration-300 group-hover:scale-105" src="https://media.istockphoto.com/id/1587604256/photo/portrait-lawyer-and-black-woman-with-tablet-smile-and-happy-in-office-workplace-african.jpg?s=170667a&w=is&k=20&c=LOfI0wy7T0jFnKYmMOmNYYnqH1v9nLCD3pCCau10_bg=" alt="" />
+             <div className="flex items-center justify-center gap-2 absolute top-40 left-20 ">
+               <span className="w-8 h-8 rounded-full border-2 flex items-center justify-center bg-white hover:bg-green-500 "> <FaFacebook className="text-green-700"/> </span>  
+               <span className="w-8 h-8 rounded-full border-2  flex items-center justify-center bg-white hover:bg-green-500 "> <FiTwitter className="text-green-700"/> </span>  
+               <span className="w-8 h-8 rounded-full border-2  flex items-center justify-center bg-white hover:bg-green-500 "> <FaInstagram className="text-green-700"/> </span>  
+                  
+             </div>
+            <div className="text-center my-8">
+             <h4 className="font-bold">Full Name</h4>
+             <h5 className="text-gray-700">Destination</h5>
+             </div>
+          </div>
+          <div className=" relative shadow-lg bg-white group overflow-hidden">
+             <img className=" transition-transform duration-300 group-hover:scale-105" src="https://www.shutterstock.com/image-photo/confident-middle-aged-business-man-260nw-2516789501.jpg" alt="" />
+             <div className="flex items-center justify-center gap-2 absolute top-40 left-20 ">
+               <span className="w-8 h-8 rounded-full border-2 flex items-center justify-center bg-white hover:bg-green-500 "> <FaFacebook className="text-green-700"/> </span>  
+               <span className="w-8 h-8 rounded-full border-2  flex items-center justify-center bg-white hover:bg-green-500 "> <FiTwitter className="text-green-700"/> </span>  
+               <span className="w-8 h-8 rounded-full border-2  flex items-center justify-center bg-white hover:bg-green-500 "> <FaInstagram className="text-green-700"/> </span>  
+                  
+             </div>
+            <div className="text-center my-8">
+             <h4 className="font-bold">Full Name</h4>
+             <h5 className="text-gray-700">Destination</h5>
+             </div>
+          </div>
+          <div className=" relative shadow-lg bg-white group overflow-hidden">
+             <img className=" transition-transform duration-300 group-hover:scale-105" src="https://media.istockphoto.com/id/1503232125/photo/happy-smile-and-portrait-of-business-man-in-city-for-professional-corporate-and-pride.jpg?s=612x612&w=0&k=20&c=uPu3GKHW507ruenLY8xwsB3OgOSwLSWjE3fBpy20Hac=" alt="" />
+             <div className="flex items-center justify-center gap-2 absolute top-39 left-20 ">
+               <span className="w-8 h-8 rounded-full border-2 flex items-center justify-center bg-white hover:bg-green-500 "> <FaFacebook className="text-green-700"/> </span>  
+               <span className="w-8 h-8 rounded-full border-2  flex items-center justify-center bg-white hover:bg-green-500 "> <FiTwitter className="text-green-700"/> </span>  
+               <span className="w-8 h-8 rounded-full border-2  flex items-center justify-center bg-white hover:bg-green-500 "> <FaInstagram className="text-green-700"/> </span>  
+                  
+             </div>
+            <div className="text-center my-8">
+             <h4 className="font-bold">Full Name</h4>
+             <h5 className="text-gray-700">Destination</h5>
+             </div>
+          </div>
+         
+         </div>
       </section>
     </>
   );
